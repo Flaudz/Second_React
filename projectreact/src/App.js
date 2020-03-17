b6155a78390579ce88bfd4a59811f7cae39d23a7
Flaudz <nico936d@edu.campusvejle.dk>
2020-03-17 14:13:01 +0100

    Added removed static and replaced it with something felxi flexi (name and message in tweet.js and app.js) ps: Added Tweet.js

--- projectreact/src/App.js
+++ projectreact/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
-import Hello from "./sayHello";
+import Tweet from "./Tweet";
 
 function App() {
 	return (
 		<div>
-			<Hello />
+			<Tweet name="Jimmy" message="Wtf" />
+			<Tweet name="Nicolaj" message="I love you<3" />
+			<Tweet name="Christian" message="Im white theme boy ;)" />
+			<Tweet name="Lucas" message="Year" />
 		</div>
 	);
 }
